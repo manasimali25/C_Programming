e55c2e01b65697a4f9289ed1c6e430e37d39b911
Manasi Mali <manasimali06@gmail.com>
2026-05-27 13:53:13 +0530

    Add files via upload

---
+++ CheckVowel.c
@@ -0,0 +1,50 @@
+// Question: Accept one character from user and check whether that character is vowel (a, e, i, o, u) or not.
+
+// Input: e
+// Output: False
+
+// Input: E
+// Output: True
+
+#include<stdio.h>
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 2
+
+BOOL ChkVowel(char ch)
+{
+  if((ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u') || (ch == 'A') || (ch == 'E') || (ch == 'I') || (ch == 'O') || (ch == 'U'))
+  {
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
+
+}
+
+int main()
+{
+  char cValue = '\0';
+  BOOL bRet = FALSE;
+
+  printf("Enter Character: \n");
+  scanf("%c", &cValue);
+
+  bRet = ChkVowel(cValue);
+
+  if(bRet == TRUE)
+  {
+    printf("It is Vowel\n");
+  }
+  else
+  {
+    printf("It is not Vowel\n");
+  }
+
+
+  return 0;
+}
---
+++ ConvertChar.c
@@ -0,0 +1,35 @@
+// Question: Accept one character from user and convert case of that character
+
+// Input: a 
+// Output: A
+
+// Input: m
+// Output: M
+
+#include<stdio.h>
+
+void DisplayConvert(char cValue)
+{
+  if(cValue >= 'a' && cValue <= 'z')
+  {
+    printf("%c", cValue - 32);
+  }
+  else if(cValue >= 'A' && cValue <= 'Z')
+  {
+    printf("%c", cValue +32);
+  }
+
+}
+
+int main()
+{
+  char cValue = '\0';
+
+  printf("Enter Character: \n");
+  scanf("%c",&cValue);
+
+  DisplayConvert(cValue);
+
+
+  return 0;
+}

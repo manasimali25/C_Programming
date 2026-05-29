f1e8771b476950b6335e55d414ac6fd4c92c9213
Manasi Mali <manasimali06@gmail.com>
2026-05-29 14:59:26 +0530

    Add files via upload

---
+++ CountEvenDig.c
@@ -0,0 +1,51 @@
+// Question: Write a program which accept number from user and return the count of even digits.
+
+// Input: 2395
+// Output: 1
+
+// Input: -1018
+// Output: 2
+
+#include<stdio.h>
+
+int CountEven(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit % 2 == 0)
+        {
+            iCount++;
+        }
+
+        iNo = iNo / 10;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = CountEven(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
+
+// Time Complexity: O(n)
---
+++ CountOddDig.c
@@ -0,0 +1,51 @@
+// Question: Write a program which accept number from user and return the count of odd digits.
+
+// Input: 2395
+// Output: 3
+
+// Input: -1018
+// Output: 2
+
+#include<stdio.h>
+
+int CountOdd(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit % 2 != 0)
+        {
+            iCount++;
+        }
+
+        iNo = iNo / 10;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = CountOdd(iValue);
+
+    printf("%d",iRet);
+
+    return 0;
+}
+
+// Time Complexity: O(n)

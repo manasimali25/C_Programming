8a3e6ee05eb88c691953e5e6b4e3f3ea543dd7c4
Manasi Mali <manasimali06@gmail.com>
2026-05-29 12:22:21 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,43 @@
+// Question: Write a program to find factorial of given number.
+
+// Input: 5
+// Output: 120    (5 * 4 * 3 * 2 * 1)
+
+// Input: -3
+// Output: 6      (3 * 2 * 1)
+
+#include<stdio.h>
+
+int Factorial(int iNo)
+{
+    int iCnt = 0;
+    int iFact = 1;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        iFact = iFact * iCnt;
+    }
+
+    return iFact;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = Factorial(iValue);
+
+    printf("Factorial of number is %d",iRet);
+
+    return 0;
+}
+
+// Time Complexity : O(n)

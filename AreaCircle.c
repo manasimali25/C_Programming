1360384e0dce90eb2e5c84f0a0801631766369b2
Manasi Mali <manasimali06@gmail.com>
2026-05-29 13:42:21 +0530

    Add files via upload

---
+++ AreaCircle.c
@@ -0,0 +1,37 @@
+// Question: Write a program which accepts radius of circle from user and calculate its area.
+// Consider value of PI as 3.14 (Area = PI * Radius * Radius)
+
+// Input: 5.3
+// Output: 88.2026
+
+// Input: 10.4
+// Output: 339.6224
+
+#include<stdio.h>
+#define PI 3.14       // MACRO
+
+double CircleArea(float fRadius)
+{
+    double dArea = 0.0f;
+
+    dArea = PI * fRadius * fRadius;
+
+    return dArea;
+}
+
+int main()
+{
+    float fValue = 0.0;
+    double dRet = 0.0;
+
+    printf("Enter radius : ");
+    scanf("%f",&fValue);
+
+    dRet = CircleArea(fValue);
+
+    printf("Area is : %lf",dRet);
+
+    return 0;
+}
+
+// Time Complexity : O(1)

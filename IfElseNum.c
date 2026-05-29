c6e9b92dac8a7eaa30034ff5185f09a523480b73
Manasi Mali <manasimali06@gmail.com>
2026-05-29 12:06:37 +0530

    Add files via upload

---
+++ IfElseNum.c
@@ -0,0 +1,41 @@
+// Question: Write a program which accept number from user and if number is less than 50 then print small, if it is greater than 50 and less than 100 then print medium, if it is greater than 100 then print large.
+
+// Input: 75
+// Output: Medium
+
+#include<stdio.h>
+
+void Number(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= 1; iCnt++)
+    {
+        if(iNo < 50)
+        {
+            printf("Small");
+        }
+        else if((iNo >= 50) && (iNo < 100))
+        {
+            printf("Medium");
+        }
+        else
+        {
+            printf("Large");
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    Number(iValue);
+
+    return 0;
+}
+
+// Time Complexity : O(1)  bcoz we are not taking N number and loop is going only once

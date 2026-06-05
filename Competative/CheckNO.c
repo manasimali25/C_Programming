18578a0423583038765547d215ba8d1ccced7e4b
Manasi Mali <manasimali06@gmail.com>
2026-06-05 12:59:02 +0530

    Add files via upload

---
+++ Competative/CheckNO.c
@@ -0,0 +1,73 @@
+// Question: Accept N numbers from user and accept one another number as NO, and check whether NO is present or not.
+
+// Input:   N: 6
+//          NO: 12
+//          Elements: 8 11 34 21 13 44
+// Output: FALSE
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL Check(int Arr[], int iLength, int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0, iValue = 0;
+    int *p = NULL;
+    BOOL bRet = FALSE;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    printf("Enter NO : ");
+    scanf("%d",&iValue);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements\n",iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    bRet = Check(p,iSize,iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("NO is present");
+    }
+    else
+    {
+        printf("NO is not present");
+    }
+
+    free(p);
+
+    return 0;
+}
